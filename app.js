4e75e4597af36c835f86f73be4776cab4fdfded6
Sille Van Landschoot <info@sillevl.be>
2015-11-26 20:48:34 +0100

    dynamic host and ip

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ var coap = require('coap');
 
 program
   .version('1.0.1')
-  .option('-s, --host', 'hostname for the mbed')
-  .option('-p, --port', 'hostname for the mbed')
+  .usage('command [settings] hostname')
+  .option('-s, --host <hostname>', 'coap server hostname', 'localhost')
+  .option('-p, --port <port>', 'coap server port', 5683 )
+
+
 
 program
   .command('led [color]')
   .description('set or get the led color')
   .action(function(color, options){
-    var options = {
-      hostname: '192.168.1.29',
+    var coapOptions = {
+      hostname: program.host,
+      port: program.port,
       method: 'POST',
       pathname: '/led'
     }
 
-    if(typeof color === 'undefined') options.method = 'GET';
+    if(typeof color === 'undefined') coapOptions.method = 'GET';
 
-    var req = coap.request(options)
-    if(options.method === 'GET'){
+    var req = coap.request(coapOptions)
+    if(coapOptions.method === 'GET'){
       req.on('response', function(res) {
         console.log("Led color: %s", res.payload.toString());
       })
@@ -33,12 +37,13 @@ program
   .command('temperature')
   .description('get the temperature')
   .action(function(options){
-    var options = {
-      hostname: '192.168.1.29',
+    var coapOptions = {
+      hostname: program.host,
+      port: program.port,
       method: 'GET',
       pathname: '/temperature'
     }
-    var req = coap.request(options)
+    var req = coap.request(coapOptions)
     req.on('response', function(res) {
       console.log("Temperature: %d°C", res.payload.toString());
     })
@@ -49,12 +54,13 @@ program
   .command('beep <frequency>')
   .description('beep the mbed with a given frequency')
   .action(function(frequency, options){
-    var options = {
-      hostname: '192.168.1.29',
+    var coapOptions = {
+      hostname: program.host,
+      port: program.port,
       method: 'POST',
       pathname: '/beep'
     }
-    var req = coap.request(options);
+    var req = coap.request(coapOptions);
     req.end(frequency);
   });
 
